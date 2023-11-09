47042498e329bd181937475c559ab6676662b824
AnasDr <me9e98.2012@gmail.com>
2023-11-09 09:52:33 +0300

    some changes

--- src/routes/groups/RegisterationPages/LoginPage.jsx
+++ src/routes/groups/RegisterationPages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { UserOutlined, LockOutlined, LoginOutlined } from "@ant-design/icons";
-import { Button, Image, Input, Space, Typography, message } from "antd";
+import { Button, Form, Image, Input, Space, Typography, message } from "antd";
 import React, { useState } from "react";
 import cloudImage from "../../../assets/cloud.png";
 import { Link } from "react-router-dom";
@@ -29,11 +29,14 @@ const LoginPage = () => {
       content: "The user name or password is incorrect",
     });
   };
-  const buttomClicked = () => {
+
+  const onFinish = (values) => {
     setLoading(true);
 
-    // Call API
-    setLoading(false);
+    setTimeout(() => {
+      success();
+      setLoading(false);
+    }, 4000);
   };
   return (
     <div className="login-page">
@@ -59,71 +62,92 @@ const LoginPage = () => {
         </Typography.Title>
       </Space>
       <div className="login-page-content">
-        <Space size={24} direction="vertical">
-          <Space size={10} direction="vertical">
-            <Space
-              style={{ width: "25vw" }}
-              align="center"
-              direction="vertical"
-              className="login-header"
-            >
-              <Image preview={false} width={150} src={cloudImage}></Image>
-              <Typography.Title level={2}>Welcome Back</Typography.Title>
-              <Typography.Paragraph>
-                Please sign in to continue.
-              </Typography.Paragraph>
-            </Space>
-            <Input
-              disabled={loading}
-              style={{
-                width: "25vw",
-                height: "40px",
-              }}
-              placeholder="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-              prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-            />
-
-            <Input.Password
-              status={""}
-              disabled={loading}
-              style={{
-                width: "25vw",
-                height: "40px",
-              }}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              onPressEnter={() => {
-                // TODO
-              }}
-              placeholder="passowrd"
-              prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-            />
+        <Space size={10} direction="vertical">
+          <Space
+            style={{ width: "25vw" }}
+            align="center"
+            direction="vertical"
+            className="login-header"
+          >
+            <Image preview={false} width={150} src={cloudImage}></Image>
+            <Typography.Title level={2}>Welcome Back</Typography.Title>
+            <Typography.Paragraph>
+              Please sign in to continue.
+            </Typography.Paragraph>
           </Space>
 
-          <Button
-            onClick={buttomClicked}
-            loading={loading}
-            icon={<LoginOutlined />}
-            className="custom-button"
-            block
-          >
-            Login
-          </Button>
-          <Typography.Text
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            Don't have an account?
-            <span style={{ marginLeft: "3px" }}>
-              <Link to={"/register"}> Signup now</Link>
-            </span>
-          </Typography.Text>
+          <Form onFinish={onFinish}>
+            <Form.Item
+              style={{ marginBottom: 15 }}
+              name={"username"}
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your username",
+                },
+              ]}
+            >
+              <Input
+                disabled={loading}
+                style={{
+                  width: "25vw",
+                  height: "40px",
+                }}
+                placeholder="Username"
+                prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name={"password"}
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter your password",
+                },
+                {
+                  min: 6,
+                  message: "Password must be at least 6 characters long",
+                },
+              ]}
+            >
+              <Input.Password
+                status={""}
+                disabled={loading}
+                style={{
+                  width: "25vw",
+                  height: "40px",
+                }}
+                placeholder="passowrd"
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              />
+            </Form.Item>
+
+            <Form.Item style={{ marginBottom: 15 }}>
+              <Button
+                loading={loading}
+                icon={<LoginOutlined />}
+                className="custom-button"
+                block
+                htmlType="submit"
+              >
+                Login
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Typography.Text
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                Don't have an account?
+                <span style={{ marginLeft: "3px" }}>
+                  <Link to={"/register"}> Signup now</Link>
+                </span>
+              </Typography.Text>
+            </Form.Item>
+          </Form>
         </Space>
       </div>
     </div>
