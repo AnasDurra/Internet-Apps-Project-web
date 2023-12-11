bb3013d8216e67a5a00d086bfddceef1e51c744b
gabsonia_ANAS <anas.reish.77@gmail.com>
2023-12-12 01:36:51 +0300

    cleanups

--- src/app/store middlewares/api-error.js
+++ src/app/store middlewares/api-error.js
@@ -3,16 +3,15 @@ import { errorMessage } from '../../components/messages.api';
 import { router } from '../../main';
 
 const apiError = (store) => (next) => async (action) => {
-  var response = await next(action);
+  const response = await next(action);
+  const { status, data } = response.payload || {};
 
-  if (response.payload?.status === 401 || response.payload?.status === 403) {
+  if (status === 401 || status === 403) {
     if (!window.location.pathname.startsWith('/login')) {
       router.navigate('/login', { replace: true });
-    } else {
-      errorMessage({ content: response.payload.data?.message });
-    }
-  } else if (response.payload?.status > 299) {
-    errorMessage({ content: response.payload.data?.message });
+    } else errorMessage({ content: data?.message });
+  } else if (status > 299) {
+    errorMessage({ content: data?.message });
   }
 
   return response;
--- src/app/store middlewares/auth-middleware.js
+++ src/app/store middlewares/auth-middleware.js
@@ -5,6 +5,7 @@ import { router } from '../../main';
 
 const authMiddleware = (store) => (next) => async (action) => {
   var response = await next(action);
+  
   if (
     (response.payload?.status == 401 || response.payload?.state == 403) &&
     Cookies.get('refreshToken')
@@ -12,6 +13,7 @@ const authMiddleware = (store) => (next) => async (action) => {
     const refreshToken = Cookies.get('refreshToken');
     Cookies.remove('refreshToken');
     store.dispatch(auth.endpoints.refresh.initiate(refreshToken));
+
     response = await next(action);
     if (
       (response.payload?.status == 401 || response.payload?.state == 403) &&
